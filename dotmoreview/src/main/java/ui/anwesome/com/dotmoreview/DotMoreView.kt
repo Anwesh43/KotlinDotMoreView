8a1ea78208cc3e4a186019418ec32addd45667b2
Anwesh43 <anweshthecool0@gmail.com>
2018-02-04 03:07:10 +0530

    adding DotState

--- dotmoreview/src/main/java/ui/anwesome/com/dotmoreview/DotMoreView.kt
+++ dotmoreview/src/main/java/ui/anwesome/com/dotmoreview/DotMoreView.kt
@@ -21,13 +21,30 @@ class DotMoreView(ctx:Context):View(ctx) {
     }
     data class Dot(var i:Int) {
         fun draw(canvas:Canvas, paint:Paint, gap:Float,y:Float) {
-            canvas.drawCircle(i*gap+gap,y,gap/5,paint)
+            canvas.drawCircle((i-1)*gap+gap,y,gap/5,paint)
         }
         fun update(stopcb:(Float)->Unit) {
 
         }
         fun startUpdating(startcb:(Float)->Unit) {
 
+        }
+    }
+    data class DotState(var scale:Float = 0f,var dir:Float = 0f,var prevScale:Float = 0f) {
+        fun update(stopcb:(Float)->Unit) {
+            scale += 0.1f*dir
+            if(Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                this.dir = 0f
+                prevScale = scale
+                stopcb(scale)
+            }
+        }
+        fun startUpdating(startcb:()->Unit) {
+            if(dir == 0f) {
+                dir = 1 - 2*scale
+                startcb()
+            }
         }
     }
 }
