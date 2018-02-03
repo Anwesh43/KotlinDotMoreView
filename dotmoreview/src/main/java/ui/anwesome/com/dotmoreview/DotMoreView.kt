0b2d3be47051697d1d33a17fe88fa8487a62cfe1
Anwesh43 <anweshthecool0@gmail.com>
2018-02-04 03:40:31 +0530

    adding Animator class

--- dotmoreview/src/main/java/ui/anwesome/com/dotmoreview/DotMoreView.kt
+++ dotmoreview/src/main/java/ui/anwesome/com/dotmoreview/DotMoreView.kt
@@ -100,6 +100,30 @@ class DotMoreView(ctx:Context,var n:Int = 5):View(ctx) {
             }
         }
     }
+    data class Animator(var view:View,var animated:Boolean = false) {
+        fun animate(updatecb:()->Unit) {
+            if(animated) {
+                updatecb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch(ex: Exception) {
+
+                }
+            }
+        }
+        fun start() {
+            if(!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+        fun stop() {
+            if(animated) {
+                animated = false
+            }
+        }
+    }
 }
 fun ConcurrentLinkedQueue<DotMoreView.Dot>.at(i:Int):DotMoreView.Dot? {
     var j = 0
