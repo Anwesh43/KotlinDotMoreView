3052261cf8027fdc0875f767c6f1fa97cda80a53
Anwesh43 <anweshthecool0@gmail.com>
2018-02-04 18:14:11 +0530

    using listener in Activity

--- app/src/main/java/ui/anwesome/com/kotlindotmoreview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlindotmoreview/MainActivity.kt
@@ -3,13 +3,19 @@ package ui.anwesome.com.kotlindotmoreview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import ui.anwesome.com.dotmoreview.DotMoreView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DotMoreView.create(this)
+        val view = DotMoreView.create(this)
+        view.addDotMoreListener({
+            Toast.makeText(this,"expanded",Toast.LENGTH_SHORT).show()
+        },{
+            Toast.makeText(this,"collapsed",Toast.LENGTH_SHORT).show()
+        })
         fullScreen()
     }
 }
