79a1e529fa17c028d157746772f9eace3a0f4327
Anwesh43 <anweshthecool0@gmail.com>
2018-02-04 14:44:53 +0530

    adding DotMoreView in Activity

--- app/src/main/java/ui/anwesome/com/kotlindotmoreview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlindotmoreview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlindotmoreview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.dotmoreview.DotMoreView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DotMoreView.create(this)
     }
 }
